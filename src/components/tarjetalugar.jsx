a87d1fc20ef317598abf618bdf6561267640cd61
edupanpan <eduardo20032110@gmail.com>
2024-10-17 00:01:46 -0300

    dasd

--- src/components/tarjetalugar.jsx
+++ src/components/tarjetalugar.jsx
@@ -12,53 +12,53 @@ const TarjetaLugar = ({ lugares, editarLugar, eliminarLugar }) => {
   }, [lugares]);
 
   return (
-    <Container>
+    <Container >
       <Row>
         {listaLugares.map((lugar) => (
-            <Col key={lugar.id} xs={12} md={6} lg={3} className="mb-4">
-              <div className="bg-white h-100 p-3">
-                <Row className="d-flex justify-content-between align-items-center">
-                  <Row xs="auto">
-                    <DropdownButton
-                      variant="link"
-                      id={`dropdown-${lugar.id}`}
-                      className="p-0"
+          <Col key={lugar.id} xs={12} md={6} lg={4} className="mb-4">
+            <div className="bg-white h-100 p-3">
+              <Row className="d-flex justify-content-between align-items-center">
+                <Col xs={1}>
+                  <DropdownButton
+                    variant="link"
+                    id={`dropdown-${lugar.id}`}
+                    className="p-0"
+                  >
+                    <Dropdown.Item eventKey="1" onClick={() => editarLugar(lugar)}>
+                      Editar
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      eventKey="2"
+                      onClick={() => eliminarLugar(lugar.id)}
                     >
-                      <Dropdown.Item eventKey="1" onClick={() => editarLugar(lugar)}>
-                        Editar
-                      </Dropdown.Item>
-                      <Dropdown.Item
-                        eventKey="2"
-                        onClick={() => eliminarLugar(lugar.id)}
-                      >
-                        Eliminar
-                      </Dropdown.Item>
-                    </DropdownButton>
-                  </Row>
-                  <Row xs="auto">
-                    <h2 className="m-0 text-center">{lugar.nombre}</h2>
-                  </Row>
-                </Row>
+                      Eliminar
+                    </Dropdown.Item>
+                  </DropdownButton>
+                </Col>
+              </Row>
+              <Row className='d-flex justify-content-between align-items-center text-center'>
+                <h2>{lugar.nombre}</h2>
+              </Row>
 
-                <Row className="text-center mt-3">
-                  <Col>
-                    <img
-                      src={lugar.imagen}
-                      alt={lugar.nombre}
-                      className="img-fluid"
-                    />
-                  </Col>
-                </Row>
+              <Row className="text-center mt-3">
+                <Col>
+                  <img
+                    src={lugar.imagen}
+                    alt={lugar.nombre}
+                    className="img-fluid tamaño"
+                  />
+                </Col>
+              </Row>
 
-                <Row className="text-center mt-2">
-                  <Col>
-                    <p>{lugar.fechavisita}</p>
-                    <p>{lugar.direccion}</p>
-                  </Col>
-                </Row>
-              </div>
-            </Col>
-          ))}
+              <Row className="text-center mt-2">
+                <Col>
+                  <p>{lugar.fechavisita}</p>
+                  <p>{lugar.direccion}</p>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
