fb50552498cb6ed15a7bff5e92a1df236120d76b
edupanpan <eduardo20032110@gmail.com>
2024-10-16 22:50:28 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route } from 'react-router-dom'
+import {HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Album from './components/pages/Album'
 import Mapa from './components/pages/Mapa'
@@ -7,11 +7,13 @@ import Mapa from './components/pages/Mapa'
 const App = () => {
   return (
     <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/album" element={<Album />} />
         <Route path="/mapa" element={<Mapa />} />
       </Routes>
+    </Router>
     </>        
   )
 }
